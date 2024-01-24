1b592807a43f0c4ca674bf08a97b643519f5d377
Ferdinand Thiessen <opensource@fthiessen.de>
2024-01-24 16:44:25 +0100

    fix: Do not rely on `OC.webroots` or `OC.appwebroots` but use own logic
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/oc.d.ts
+++ lib/oc.d.ts
@@ -1,5 +1,9 @@
 /// <reference types="@nextcloud/typings" />
 
 declare interface Window {
-    OC: Nextcloud.v25.OC | Nextcloud.v26.OC | Nextcloud.v27.OC;
+    OC: Nextcloud.v26.OC | Nextcloud.v27.OC;
+
+    // Private state directly from server
+    _oc_webroot?: string
+    _oc_appswebroots?: Record<string, string|undefined>
 }
