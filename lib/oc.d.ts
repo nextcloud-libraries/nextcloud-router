f1df73b9d5178f70b7ffff1129f3630152a0ac28
Ferdinand Thiessen <opensource@fthiessen.de>
2024-01-24 21:19:59 +0100

    fix: Adjust linting
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/oc.d.ts
+++ lib/oc.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="@nextcloud/typings" />
 
 declare interface Window {
-    OC: Nextcloud.v26.OC | Nextcloud.v27.OC;
+	OC: Nextcloud.v26.OC | Nextcloud.v27.OC;
 
-    // Private state directly from server
-    _oc_webroot?: string
-    _oc_appswebroots?: Record<string, string|undefined>
+	// Private state directly from server
+	_oc_webroot?: string
+	_oc_appswebroots?: Record<string, string|undefined>
 }
